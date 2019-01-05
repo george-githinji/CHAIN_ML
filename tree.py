bbb7c1bf13e3073ceb07c1816b0c85d17ed5520d
Narshion Ngao <nngao@kemri-wellcome.org>
2019-01-05 17:28:14 +0300

    first commit

---
+++ tree.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  5 16:09:04 2019
+
+@author: nngao
+"""
+
+from sklearn import tree
+
+X = [[0, 0], [1, 1]]
+Y = [0, 1]
+clf = tree.DecisionTreeClassifier()
